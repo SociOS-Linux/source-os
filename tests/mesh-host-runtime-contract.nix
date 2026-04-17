bc2ec3e643f7b4c2973bf10d8f6e84b178616d6b
mdheller <21163552+mdheller@users.noreply.github.com>
2026-04-17 18:30:55 -0400

    test(source-os): add mesh host runtime contract check

---
+++ tests/mesh-host-runtime-contract.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.runCommand "mesh-host-runtime-contract" {
+  nativeBuildInputs = [ pkgs.gnugrep ];
+} ''
+  grep -q 'specialArgs = { inherit self; };' ${../flake.nix}
+  grep -q '{ self, pkgs, ... }:' ${../hosts/canary-x86_64/default.nix}
+  grep -q '{ self, pkgs, ... }:' ${../hosts/stable-x86_64/default.nix}
+  grep -q 'sourceos.mesh.runtime = {' ${../hosts/canary-x86_64/default.nix}
+  grep -q 'sourceos.mesh.runtime = {' ${../hosts/stable-x86_64/default.nix}
+  grep -q 'meshdPackage = self.packages.${pkgs.system}.meshd;' ${../hosts/canary-x86_64/default.nix}
+  grep -q 'linkdPackage = self.packages.${pkgs.system}.meshd-linkd;' ${../hosts/canary-x86_64/default.nix}
+  grep -q 'meshdPackage = self.packages.${pkgs.system}.meshd;' ${../hosts/stable-x86_64/default.nix}
+  grep -q 'linkdPackage = self.packages.${pkgs.system}.meshd-linkd;' ${../hosts/stable-x86_64/default.nix}
+  mkdir -p $out
+  echo validated > $out/result.txt
+''
